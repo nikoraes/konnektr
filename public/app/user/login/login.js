30a95cf9f50ab1029c8a75a43ec7950b9f4d6433
Niko Raes <niko.raes@gmail.com>
2016-03-06 19:07:52 +0000

    test

--- public/app/user/login/login.js
+++ public/app/user/login/login.js
@@ -19,19 +19,21 @@ angular.module('konnektr.user.login', [])
 		  
 			$scope.server = cornercouch("https://7e0ec13a-1e6e-4efe-83ed-709d223d5e37-bluemix.cloudant.com", "GET");
 			$scope.server.session();
+			$scope.konnektrdata = $scope.server.getDB('konnektr-data');
 			
 			$scope.submitLogin = function() {
-			    $scope.server.login($scope.user.username, $scope.user.password)
-			        .success( function() {
-			            $scope.user.username = $scope.user.password = '';
-			            $scope.showInfo = true;
-			            $scope.server.getInfo();
-			            $scope.server.getDatabases();
-			            $scope.server.getUUIDs(3);
-			            $scope.server.getUserDoc();
-			            $scope.server.getUserDB();
-			        });
-			    };	  
+				$scope.server.login($scope.user.username, $scope.user.password)
+					.success( function() {
+	          $scope.user.username = $scope.user.password = '';
+	          $scope.showInfo = true;
+	          $scope.server.getInfo();
+	          $scope.server.getDatabases();
+	          $scope.server.getUUIDs(3);
+	          $scope.server.getUserDoc();
+	          $scope.server.getUserDB();
+					});
+	    };	
+	    $scope.testdoc = $scope.konnektrdata.getDoc("nikoraes-openhab-00000");
 		}])
 			
 ;	
