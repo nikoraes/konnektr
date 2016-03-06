bf018b2358b50d8293363b70883258377215702b
Niko Raes <niko.raes@gmail.com>
2016-03-06 12:54:10 +0000

    test

--- public/app/common/loading.js
+++ public/app/common/loading.js
@@ -3,14 +3,12 @@
  * @see http://tobiasahlin.com/spinkit/
  */
 
-angular
-    .module('konnektr.common')
-    .directive('rdLoading', rdLoading);
+angular.module('konnektr.common', [])
 
-function rdLoading() {
-    var directive = {
-        restrict: 'AE',
-        template: '<div class="loading"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>'
-    };
-    return directive;
-};
+    .directive('rdLoading', function() {
+		    var directive = {
+		        restrict: 'AE',
+		        template: '<div class="loading"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>'
+		    };
+		    return directive;
+		});
